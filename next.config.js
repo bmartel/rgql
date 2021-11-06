3965d75075d13c5c56333134f9eaeaf1a0811ca1
brandonmartel <brandonmartel@gmail.com>
2021-11-06 12:51:50 -0500

    set scalable meta, add link to next-pwa

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const withPwa = require('next-pwa')
 /** @type {import('next').NextConfig} */
 module.exports = withPwa({
   reactStrictMode: true,
+  // For configuration options @see https://github.com/shadowwalker/next-pwa
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+  },
 })
